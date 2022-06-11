e5bbb4459d42e26950cea38c93961cb210a853bc
Simon Dean <simon@simondean.org>
2022-06-11 01:07:23 +0100

    Use older javax dependency

--- gradle-static-analyzer/src/main/java/tech/kronicle/gradlestaticanalyzer/internal/models/mavenxml/Metadata.java
+++ gradle-static-analyzer/src/main/java/tech/kronicle/gradlestaticanalyzer/internal/models/mavenxml/Metadata.java
@@ -1,11 +1,12 @@
 package tech.kronicle.gradlestaticanalyzer.internal.models.mavenxml;
 
-import jakarta.xml.bind.annotation.XmlAccessType;
-import jakarta.xml.bind.annotation.XmlAccessorType;
-import jakarta.xml.bind.annotation.XmlElement;
-import jakarta.xml.bind.annotation.XmlRootElement;
 import lombok.Data;
 import tech.kronicle.gradlestaticanalyzer.internal.models.mavenxml.metadata.Versioning;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 
 @Data
 @XmlRootElement
